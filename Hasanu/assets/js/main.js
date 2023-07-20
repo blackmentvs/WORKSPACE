4df11dc6bf942c821477d9575468c5487dda7e02
Sơn Trần Viết <blackmentvs@gmail.com>
2023-07-21 00:21:02 +0700

    order js fix cart

--- Hasanu/assets/js/main.js
+++ Hasanu/assets/js/main.js
@@ -1,4 +1,6 @@
-//booking
+// 
+// ------------------------------booking----------------------
+// 
 const bookBtns = document.querySelectorAll('.js-booking')
 const modal = document.querySelector('.js-modal')
 const modalContainer = document.querySelector('.js-modal-container')
@@ -19,36 +21,11 @@ modalContainer.addEventListener('click', function(event) {
 function closeBuytickets() {
     modal.classList.remove('open')
 }
-//end booking
-//begin menubar
-
-// var header = document.getElementById('header')
-// var menubars = document.getElementById('menu-bars')
-// menubars.onclick = function() {
-//     var iClose = header.clientHeight === 47;
-//     if (iClose) {
-//         header.style.height = 'auto';
-//     } else {
-//         header.style.height = '47px';
-//     }
-// }
-// menuItems = document.querySelectorAll('#nav li a[href*="#"]')
-// for (var i = 0; i < menuItems.length; i++) {
-//     var menuItem = menuItems[i];
-//     menuItem.onclick = function(event) {
-//         var isParentMenu = this.nextElementSibling && this.nextElementSibling.classList.contains('subnav');
-//         if (isParentMenu) {
-//             event.preventDefault();
-//         } else {
-//             header.style.height = null;
-//         }
-//     }
-// }
-//end bar
+// 
+//----------------------------add Cart----------------------
 // 
 (function() {
     var cart = document.getElementsByClassName('js-cd-cart');
-    console.log('ten class', cart);
     if (cart.length > 0) {
         var cartAddBtns = document.getElementsByClassName('js-cd-add-to-cart'),
             cartBody = cart[0].getElementsByClassName('cd-cart__body')[0],
@@ -147,8 +124,8 @@ function closeBuytickets() {
 
         function addProduct(target) {
 
-            productId = productId + 1;
-            var productAdded = '<li class="cd-cart__product"><div class="cd-cart__image"><a href="#0"><img src="assets/img/product-preview.png" alt="placeholder"></a></div><div class="cd-cart__details"><h3 class="truncate"><a href="#0">Product Name</a></h3><span class="cd-cart__price">$25.99</span><div class="cd-cart__actions"><a href="#0" class="cd-cart__delete-item">Delete</a><div class="cd-cart__quantity"><label for="cd-product-' + productId + '">Qty</label><span class="cd-cart__select"><select class="reset" id="cd-product-' + productId + '" name="quantity"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option></select><svg class="icon" viewBox="0 0 12 12"><polyline fill="none" stroke="currentColor" points="2,4 6,8 10,4 "/></svg></span></div></div></div></li>';
+            productId = 1;
+            var productAdded = '<li class="cd-cart__product"><div class="cd-cart__image"><a href="#0"><img src="assets/images/mon1.jpg" alt="placeholder"></a></div><div class="cd-cart__details"><h3 class="truncate"><a href="#0">Product Name</a></h3><span class="cd-cart__price">$25.99</span><div class="cd-cart__actions"><a href="#0" class="cd-cart__delete-item">Delete</a><div class="cd-cart__quantity"><label for="cd-product-' + productId + '">Qty</label><span class="cd-cart__select"><select class="reset" id="cd-product-' + productId + '" name="quantity"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option></select><svg class="icon" viewBox="0 0 12 12"><polyline fill="none" stroke="currentColor" points="2,4 6,8 10,4 "/></svg></span></div></div></div></li>';
             cartList.insertAdjacentHTML('beforeend', productAdded);
         };
 
@@ -175,7 +152,7 @@ function closeBuytickets() {
             }, 8000);
         };
 
-        function removePreviousProduct() { // definitively removed a product from the cart (undo not possible anymore)
+        function removePreviousProduct() {
             var deletedProduct = cartList.getElementsByClassName('cd-cart__product--deleted');
             if (deletedProduct.length > 0) deletedProduct[0].remove();
         };
@@ -237,46 +214,3 @@ function closeBuytickets() {
         };
     }
 })();
-// ------------------------------------------------
-// ------------------------------------------------
-function initCartEvents() {
-    for (var i = 0; i < cartAddBtns.length; i++) {
-        (function(i) {
-            cartAddBtns[i].addEventListener('click', addToCart);
-        })(i);
-    }
-
-    cart[0].getElementsByClassName('cd-cart__trigger')[0].addEventListener('click', function(event) {
-        event.preventDefault();
-        toggleCart();
-    });
-
-    cart[0].addEventListener('click', function(event) {
-        if (event.target == cart[0]) {
-            toggleCart(true);
-        } else if (event.target.closest('.cd-cart__delete-item')) {
-            event.preventDefault();
-            removeProduct(event.target.closest('.cd-cart__product'));
-        }
-    });
-
-    cart[0].addEventListener('change', function(event) {
-        if (event.target.tagName.toLowerCase() == 'select') quickUpdateCart();
-    });
-
-    cartUndo.addEventListener('click', function(event) {
-        if (event.target.tagName.toLowerCase() == 'a') {
-            event.preventDefault();
-            if (cartTimeoutId) clearInterval(cartTimeoutId);
-            var deletedProduct = cartList.getElementsByClassName('cd-cart__product--deleted')[0];
-            Util.addClass(deletedProduct, 'cd-cart__product--undo');
-            deletedProduct.addEventListener('animationend', function cb() {
-                deletedProduct.removeEventListener('animationend', cb);
-                Util.removeClass(deletedProduct, 'cd-cart__product--deleted cd-cart__product--undo');
-                deletedProduct.removeAttribute('style');
-                quickUpdateCart();
-            });
-            Util.removeClass(cartUndo, 'cd-cart__undo--visible');
-        }
-    });
-};
