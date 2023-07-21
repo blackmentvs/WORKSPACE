41bfb0bb70601a928d5afe662419c3fd7303b112
Sơn Trần Viết <blackmentvs@gmail.com>
2023-07-22 00:15:18 +0700

    fix cart

--- Hasanu/assets/js/main.js
+++ Hasanu/assets/js/main.js
@@ -24,193 +24,143 @@ function closeBuytickets() {
 // 
 //----------------------------add Cart----------------------
 // 
-(function() {
-    var cart = document.getElementsByClassName('js-cd-cart');
-    if (cart.length > 0) {
-        var cartAddBtns = document.getElementsByClassName('js-cd-add-to-cart'),
-            cartBody = cart[0].getElementsByClassName('cd-cart__body')[0],
-            cartList = cartBody.getElementsByTagName('ul')[0],
-            cartListItems = cartList.getElementsByClassName('cd-cart__product'),
-            cartTotal = cart[0].getElementsByClassName('cd-cart__checkout')[0].getElementsByTagName('span')[0],
-            cartCount = cart[0].getElementsByClassName('cd-cart__count')[0],
-            cartCountItems = cartCount.getElementsByTagName('li'),
-            cartUndo = cart[0].getElementsByClassName('cd-cart__undo')[0],
-            productId = 0, //this is a placeholder -> use your real product ids instead
-            cartTimeoutId = false,
-            animatingQuantity = false;
-        initCartEvents();
+const btnCart = document.querySelector('#cart-icon');
+const cart = document.querySelector('.cart');
+const btnClose = document.querySelector('#cart-close');
+
+btnCart.addEventListener('click', () => {
+    cart.classList.add('cart-active');
+});
+
+btnClose.addEventListener('click', () => {
+    cart.classList.remove('cart-active');
+});
+
+document.addEventListener('DOMContentLoaded', loadFood);
+
+function loadFood() {
+    loadContent();
+
+}
+
+function loadContent() {
+    //Xóa món trong giỏ hàng
+    let btnRemove = document.querySelectorAll('.cart-remove');
+    btnRemove.forEach((btn) => {
+        btn.addEventListener('click', removeItem);
+    });
+
+    //Product Item Change Event
+    let qtyElements = document.querySelectorAll('.cart-quantity');
+    qtyElements.forEach((input) => {
+        input.addEventListener('change', changeQty);
+    });
+
+    //Product Cart
+
+    let cartBtns = document.querySelectorAll('.add-cart');
+    cartBtns.forEach((btn) => {
+        btn.addEventListener('click', addCart);
+    });
+
+    updateTotal();
+}
 
 
-        function initCartEvents() {
-            // add products to cart
-            for (var i = 0; i < cartAddBtns.length; i++) {
-                (function(i) {
-                    cartAddBtns[i].addEventListener('click', addToCart);
-                })(i);
-            }
-
-            // open/close cart
-            cart[0].getElementsByClassName('cd-cart__trigger')[0].addEventListener('click', function(event) {
-                event.preventDefault();
-                toggleCart();
-            });
-
-            cart[0].addEventListener('click', function(event) {
-                if (event.target == cart[0]) { // close cart when clicking on bg layer
-                    toggleCart(true);
-                } else if (event.target.closest('.cd-cart__delete-item')) { // remove product from cart
-                    event.preventDefault();
-                    removeProduct(event.target.closest('.cd-cart__product'));
-                }
-            });
-
-            // update product quantity inside cart
-            cart[0].addEventListener('change', function(event) {
-                if (event.target.tagName.toLowerCase() == 'select') quickUpdateCart();
-            });
-
-            //reinsert product deleted from the cart
-            cartUndo.addEventListener('click', function(event) {
-                if (event.target.tagName.toLowerCase() == 'a') {
-                    event.preventDefault();
-                    if (cartTimeoutId) clearInterval(cartTimeoutId);
-                    // reinsert deleted product
-                    var deletedProduct = cartList.getElementsByClassName('cd-cart__product--deleted')[0];
-                    Util.addClass(deletedProduct, 'cd-cart__product--undo');
-                    deletedProduct.addEventListener('animationend', function cb() {
-                        deletedProduct.removeEventListener('animationend', cb);
-                        Util.removeClass(deletedProduct, 'cd-cart__product--deleted cd-cart__product--undo');
-                        deletedProduct.removeAttribute('style');
-                        quickUpdateCart();
-                    });
-                    Util.removeClass(cartUndo, 'cd-cart__undo--visible');
-                }
-            });
-        };
-
-        function addToCart(event) {
-            event.preventDefault();
-            if (animatingQuantity) return;
-            var cartIsEmpty = Util.hasClass(cart[0], 'cd-cart--empty');
-            //update cart product list
-            addProduct(this);
-            //update number of items 
-            updateCartCount(cartIsEmpty);
-            //update total price
-            updateCartTotal(this.getAttribute('data-price'), true);
-            //show cart
-            Util.removeClass(cart[0], 'cd-cart--empty');
-        };
-
-        function toggleCart(bool) { // toggle cart visibility
-            var cartIsOpen = (typeof bool === 'undefined') ? Util.hasClass(cart[0], 'cd-cart--open') : bool;
-
-            if (cartIsOpen) {
-                Util.removeClass(cart[0], 'cd-cart--open');
-                //reset undo
-                if (cartTimeoutId) clearInterval(cartTimeoutId);
-                Util.removeClass(cartUndo, 'cd-cart__undo--visible');
-                removePreviousProduct(); // if a product was deleted, remove it definitively from the cart
-
-                setTimeout(function() {
-                    cartBody.scrollTop = 0;
-                    //check if cart empty to hide it
-                    if (Number(cartCountItems[0].innerText) == 0 || Number(cartCountItems[0].innerText) <= 0) Util.addClass(cart[0], 'cd-cart--empty');
-                }, 500);
-            } else {
-                Util.addClass(cart[0], 'cd-cart--open');
-            }
-        };
-
-        function addProduct(target) {
-
-            productId = 1;
-            var productAdded = '<li class="cd-cart__product"><div class="cd-cart__image"><a href="#0"><img src="assets/images/mon1.jpg" alt="placeholder"></a></div><div class="cd-cart__details"><h3 class="truncate"><a href="#0">Product Name</a></h3><span class="cd-cart__price">$25.99</span><div class="cd-cart__actions"><a href="#0" class="cd-cart__delete-item">Delete</a><div class="cd-cart__quantity"><label for="cd-product-' + productId + '">Qty</label><span class="cd-cart__select"><select class="reset" id="cd-product-' + productId + '" name="quantity"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option></select><svg class="icon" viewBox="0 0 12 12"><polyline fill="none" stroke="currentColor" points="2,4 6,8 10,4 "/></svg></span></div></div></div></li>';
-            cartList.insertAdjacentHTML('beforeend', productAdded);
-        };
-
-        function removeProduct(product) {
-            if (cartTimeoutId) clearInterval(cartTimeoutId);
-            removePreviousProduct(); // prduct previously deleted -> definitively remove it from the cart
-
-            var topPosition = product.offsetTop,
-                productQuantity = Number(product.getElementsByTagName('select')[0].value),
-                productTotPrice = Number((product.getElementsByClassName('cd-cart__price')[0].innerText).replace('$', '')) * productQuantity;
-
-            product.style.top = topPosition + 'px';
-            Util.addClass(product, 'cd-cart__product--deleted');
-
-            //update items count + total price
-            updateCartTotal(productTotPrice, false);
-            updateCartCount(true, -productQuantity);
-            Util.addClass(cartUndo, 'cd-cart__undo--visible');
-
-            //wait 8sec before completely remove the item
-            cartTimeoutId = setTimeout(function() {
-                Util.removeClass(cartUndo, 'cd-cart__undo--visible');
-                removePreviousProduct();
-            }, 8000);
-        };
-
-        function removePreviousProduct() {
-            var deletedProduct = cartList.getElementsByClassName('cd-cart__product--deleted');
-            if (deletedProduct.length > 0) deletedProduct[0].remove();
-        };
-
-        function updateCartCount(emptyCart, quantity) {
-            if (typeof quantity === 'undefined') {
-                var actual = Number(cartCountItems[0].innerText) + 1;
-                var next = actual + 1;
-
-                if (emptyCart) {
-                    cartCountItems[0].innerText = actual;
-                    cartCountItems[1].innerText = next;
-                    animatingQuantity = false;
-                } else {
-                    Util.addClass(cartCount, 'cd-cart__count--update');
-
-                    setTimeout(function() {
-                        cartCountItems[0].innerText = actual;
-                    }, 150);
-
-                    setTimeout(function() {
-                        Util.removeClass(cartCount, 'cd-cart__count--update');
-                    }, 200);
-
-                    setTimeout(function() {
-                        cartCountItems[1].innerText = next;
-                        animatingQuantity = false;
-                    }, 230);
-                }
-            } else {
-                var actual = Number(cartCountItems[0].innerText) + quantity;
-                var next = actual + 1;
-
-                cartCountItems[0].innerText = actual;
-                cartCountItems[1].innerText = next;
-                animatingQuantity = false;
-            }
-        };
-
-        function updateCartTotal(price, bool) {
-            cartTotal.innerText = bool ? (Number(cartTotal.innerText) + Number(price)).toFixed(2) : (Number(cartTotal.innerText) - Number(price)).toFixed(2);
-        };
-
-        function quickUpdateCart() {
-            var quantity = 0;
-            var price = 0;
-
-            for (var i = 0; i < cartListItems.length; i++) {
-                if (!Util.hasClass(cartListItems[i], 'cd-cart__product--deleted')) {
-                    var singleQuantity = Number(cartListItems[i].getElementsByTagName('select')[0].value);
-                    quantity = quantity + singleQuantity;
-                    price = price + singleQuantity * Number((cartListItems[i].getElementsByClassName('cd-cart__price')[0].innerText).replace('$', ''));
-                }
-            }
-
-            cartTotal.innerText = price.toFixed(2);
-            cartCountItems[0].innerText = quantity;
-            cartCountItems[1].innerText = quantity + 1;
-        };
+//Xóa món
+function removeItem() {
+    if (confirm('Bạn có chắc ')) {
+        let title = this.parentElement.querySelector('.cart-food-title').innerHTML;
+        itemList = itemList.filter(el => el.title != title);
+        this.parentElement.remove();
+        loadContent();
     }
-})();
+}
+
+//Thay đổi số lượng 
+function changeQty() {
+    if (isNaN(this.value) || this.value < 1) {
+        this.value = 1;
+    }
+    loadContent();
+}
+
+let itemList = [];
+
+//Thêm vào giỏ hàng
+function addCart() {
+    let food = this.parentElement;
+    let title = food.querySelector('.food-title').innerHTML;
+    let price = food.querySelector('.food-price').innerHTML;
+    let imgSrc = food.querySelector('.food-img').src;
+
+    let newProduct = { title, price, imgSrc }
+
+    //Kiểm tra xem  món có trong giỏ chưa
+    if (itemList.find((el) => el.title == newProduct.title)) {
+        alert("Món Này đã có sẵn trong giỏ hàng bạn vào thêm số lượng nhé <3");
+        return;
+    } else {
+        itemList.push(newProduct);
+    }
+
+
+    let newProductElement = createCartProduct(title, price, imgSrc);
+    let element = document.createElement('div');
+    element.innerHTML = newProductElement;
+    let cartBasket = document.querySelector('.cart-content');
+    cartBasket.append(element);
+    loadContent();
+}
+
+
+function createCartProduct(title, price, imgSrc) {
+
+    return `
+  <div class="cart-box">
+  <img src="${imgSrc}" class="cart-img">
+  <div class="detail-box">
+    <div class="cart-food-title">${title}</div>
+    <div class="price-box">
+      <div class="cart-price">${price}</div>
+       <div class="cart-amt">${price}</div>
+   </div>
+    <input type="number" value="1" class="cart-quantity">
+  </div>
+  <ion-icon name="trash" class="cart-remove"></ion-icon>
+</div>
+  `;
+}
+
+function updateTotal() {
+    const cartItems = document.querySelectorAll('.cart-box');
+    const totalValue = document.querySelector('.total-price');
+
+    let total = 0;
+
+    cartItems.forEach(product => {
+        let priceElement = product.querySelector('.cart-price');
+        let price = parseFloat(priceElement.innerHTML.replace("Rs.", ""));
+        let qty = product.querySelector('.cart-quantity').value;
+        total += (price * qty);
+        product.querySelector('.cart-amt').innerText = (price * qty * 1000).toLocaleString() + 'đ';
+
+    });
+
+    totalValue.innerHTML = (total * 1000).toLocaleString() + 'đ';
+
+
+    // Hiển thị số lượng món hiện có trên icon giỏ hàng
+
+    const cartCount = document.querySelector('.cart-count');
+    let count = itemList.length;
+    cartCount.innerHTML = count;
+
+    if (count == 0) {
+        cartCount.style.display = 'none';
+    } else {
+        cartCount.style.display = 'block';
+    }
+
+
+}
